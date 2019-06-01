edb21fb183074c8989a15edc9ac50218bc0e88ee
Fatma Zehra Çetin <37731704+fzehracetin@users.noreply.github.com>
2019-06-01 11:00:13 +0300

    Update bruteForceStringSearch.c

--- bruteForceStringSearch.c
+++ bruteForceStringSearch.c
@@ -2,13 +2,15 @@
 
 int main(int argc, char **argv)
 {
-	char str[30] = "Do i say that i need you?";
+    char str[30] = "Did i say that i need you?";
     char pattern[4] = "need";
     int k = 25, m = 4, flag = 0, i = 0, j = 0;
     
-    while ( ( !flag ) && ( i <= k - m ) ) {
+    while ( ( !flag ) && ( i <= k - m ) ) 
+    {
         j = 0;
-        while ( (j < m) && ( str[i + j] == pattern[j] ) ) { //i+j yapmazsan indis degeri bozuluyor bu onemli
+        while ( (j < m) && ( str[i + j] == pattern[j] ) ) 
+	{ //i+j yapmazsan indis degeri bozuluyor bu onemli
             j++;
         }
         if ( j == m) 
