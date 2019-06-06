6c165f98e755c2e290039e269002d0fbafc256a2
Fatma Zehra Çetin <37731704+fzehracetin@users.noreply.github.com>
2019-06-06 16:12:33 +0300

    Update boyerMooreStringSearch.c

--- boyerMooreStringSearch.c
+++ boyerMooreStringSearch.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-    int i, flag = 0, n = 25, m = 4, j, skip;
-    char str[n] = "did i say that i need you";
-    char pattern[m] = "need";
+int boyerMoore(char str[], char pattern[], int n, int m) {
     char right[26];
-    
+    int i, j, flag = 0, skip;
     
     for (i = 0; i < 26; i++) 
     {
@@ -16,17 +12,19 @@ int main(int argc, char **argv)
     {
         right[ pattern[i] - 'a' ] = i;
     }
-	while ( ( !flag ) && ( i <= n - m) ) 
-	{
+    
+    while ( ( !flag ) && ( i <= n - m) ) 
+    {
         j = m -1;
         while ( ( str[i + j] == pattern[j] ) && ( j >= 0 ) ) 
-	{
+        {
             j--;
         }
+        
         if ( j < 0 ) 
             flag = 1;
         else 
-	{
+        {
             skip = j - right[ str[ i + j] - 'a'];
             if ( skip < 0 ) 
                 i++;
@@ -34,5 +32,26 @@ int main(int argc, char **argv)
                 i = i + skip;
         }
     }
+    
+    if (!flag) 
+        i = -1;
+    
+    return i;
+}
+
+int main(int argc, char **argv)
+{
+    int n = 25, m = 4, index;
+    char str[25] = "did i say that i need you";
+    char pattern[4] = "need";
+    
+    index = boyerMoore(str, pattern, n, m);
+	
+    if (index != -1) 
+        printf("Pattern starts from index %d \n", index);
+    else 
+        printf("Pattern does not exists in the text \n");
+    
 	return 0;
 }
+
